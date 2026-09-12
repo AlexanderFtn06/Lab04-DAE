837b97afb95bdd23c9fdc198a5faf46bca9894f4
Alexander Faustino Quispe <faustinoalexander46@gmail.com>
2026-09-12 18:07:39 -0500

    feat: registrar modelos en admin y cargar datos de prueba

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Author, Book, Publisher, Category, AuthorProfile, Publication
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'nationality']
+    search_fields = ['first_name', 'last_name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'isbn', 'publication_year']
+    list_filter = ['author', 'categories']
+    filter_horizontal = ['categories']
+    search_fields = ['title', 'isbn']
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'founded_year']
+    search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+@admin.register(AuthorProfile)
+class AuthorProfileAdmin(admin.ModelAdmin):
+    list_display = ['author', 'website']
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ['book', 'publisher', 'publication_date', 'edition']
+    list_filter = ['publisher']
